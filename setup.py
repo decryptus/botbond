e22c1ed4a74e11f0fbc332a5f55ce3cc0c7bbcff
Adrien Delle Cave <adrien.delle.cave@commandersact.com>
2018-10-05 12:19:27 +0200

    [FEATURES] Added environment variables

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 from setuptools import find_packages, setup
 
 requirements = [line.strip() for line in open('requirements.txt', 'r').readlines()]
-version      = '0.14'
+version      = '0.15'
 
 if os.path.isfile('VERSION'):
     version = open('VERSION', 'r').readline().strip() or version
